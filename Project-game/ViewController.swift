9c57d1d4de813155638538f07ef5a47e94d2b23d
Sergey <dir@syakovlev.ru>
2019-11-14 18:06:12 +0500

    Initial Commit

---
+++ Project-game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project-game
+//
+//  Created by Sergey on 14/11/2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
